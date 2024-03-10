af5f00799803896b11b1643f6445e85b592a25d2
Emily <68402015+emilyyf@users.noreply.github.com>
2024-03-09 21:57:03 -0300

    Added home manager

--- flake.nix
+++ flake.nix
@@ -3,14 +3,26 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+		home-manager.url = "github:nix-community/home-manager/master";
+		home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, home-manager, ... }:
+	let
+		system = "x86_64-linux";
+	  pkgs = nixpkgs.legacyPackages.${system};
+	in {
     nixosConfigurations = {
       achird = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
+				inherit system;
         modules = [ ./configuration.nix ];
       };
     };
+		homeConfigurations = {
+      emily = home-manager.lib.homeManagerConfiguration {
+				inherit pkgs;
+        modules = [ ./home.nix ];
+      };
+		};
   };
 }
