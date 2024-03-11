bba6a7266c58e14e1d0cba21c330dded1ee57124
Emily <68402015+emilyyf@users.noreply.github.com>
2024-03-11 15:13:59 -0300

    feat: update dwm path

--- system/dwm.nix
+++ system/dwm.nix
@@ -4,13 +4,18 @@
 		rofi
 		dmenu
 		st
+		feh
 	];
 
 	services.xserver.windowManager.dwm = {
 		enable = true;
 		package = 
 			pkgs.dwm.overrideAttrs {
-				src = /home/emily/Stuff/dwm;
+				src = pkgs.fetchgit {
+					url = /home/emily/Stuff/dwm;
+					rev = "e49ea6a9b62357a899eebb0f0be3c884b2703ab3";
+					hash = "sha256-AcdlRLCd+oeewQ3fMcQpM+fQALwetE3NN3b2V2ktmLU=";
+				};
 			};
 	};
 }
