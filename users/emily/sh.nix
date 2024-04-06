63e08477460a6bc70129f4f9821a8dff58ce274c
Emily <68402015+emilyyf@users.noreply.github.com>
2024-04-06 08:08:41 -0300

    feat: better nix shell on zsh

--- users/emily/sh.nix
+++ users/emily/sh.nix
@@ -13,12 +13,32 @@
     };
   };
 
+  programs = {
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
+  };
+
   programs.zsh = {
     enable = true;
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
     defaultKeymap = "viins";
+
+    plugins = [
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.8.0";
+          sha256 = "sha256-Z6EYQdasvpl1P78poj9efnnLj7QQg13Me8x1Ryyw+dM=";
+        };
+      }
+    ];
 
     shellAliases = {
       md = "mkdir -p";
