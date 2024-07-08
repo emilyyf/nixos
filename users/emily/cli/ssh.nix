7706b1a9f1ded628b1373414308a5f14397495ca
Emily <emily@anaboth.com>
2024-07-08 19:19:01 -0300

    feat: add gitlab key

--- users/emily/cli/ssh.nix
+++ users/emily/cli/ssh.nix
@@ -7,6 +7,11 @@
         hostname = "github.com";
         identityFile = config.home.homeDirectory + "/.ssh/github";
       };
+      "gitlab.com" = {
+        user = "git";
+        hostname = "gitlab.com";
+        identityFile = config.home.homeDirectory + "/.ssh/gitlab";
+      };
     };
   };
 }
